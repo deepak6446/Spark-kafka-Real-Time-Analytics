1ce6ffe3837e6460bfbcdc6f1ed5c421564aec74
Poojari, Deepak <Deepak.Poojari@softwareag.com>
2019-07-23 19:16:49 +0530

    kafka sub support with read data

--- kafka_spark_connect.py
+++ kafka_spark_connect.py
@@ -1,43 +1,26 @@
-# from pyspark import SparkContext
-# from pyspark.streaming import StreamingContext
-# from pyspark.streaming.kafka import KafkaUtils
-
-# # Create a local StreamingContext with two working thread and batch interval of 1 second
-# sc = SparkContext("local[2]", "KafkaStreamingConsumer")
-# ssc = StreamingContext(sc, 10)
-
-
-# # kafkaStream = KafkaUtils.createStream(streamingContext, \
-# #      [ZK quorum], [consumer group id], [per-topic number of Kafka partitions to consume])
-
-# kafkaStream = KafkaUtils.createStream(ssc, "localhost:2181", "consumer-group", {"testTopic": 1})
-
-# kafkaStream.saveAsTextFiles('out.txt')
-
-# # print('Event recieved in window: ', kafkaStream.pprint())
-
-# ssc.start()
-# ssc.awaitTermination()
-
 import sys
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 if __name__ == "__main__":
 
     sc = SparkContext(appName="PythonStreamingDirectKafkaWordCount")
-    ssc = StreamingContext(sc, 0.1) #(sparkContext, Duration in sec) 
+    ssc = StreamingContext(sc, 2) #(sparkContext, Duration in sec) 
     
     brokers, topic = sys.argv[1:]
     
     kvs = KafkaUtils.createDirectStream(ssc, [topic],{"metadata.broker.list": brokers})
 
     lines = kvs.map(lambda x: x[1])
-    
+    # print("========>", lines.pprint())
     counts = lines.flatMap(lambda line: line.split(" ")) \
                   .map(lambda word: (word, 1)) \
                   .reduceByKey(lambda a, b: a+b)
-    counts.pprint()
+    counts.pprint()   
+
     ssc.start()
     ssc.awaitTermination()
